b943a62fd8667cfda5fddafcf0cd11951633f15b
MJ_Abbasi <jawadabbasi1107@gmail.com>
2025-07-14 01:11:46 -0700

    Update linear.py

--- projects/linear_regression/linear.py
+++ projects/linear_regression/linear.py
@@ -1,1 +1,18 @@
+# linear.py
+
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+# Sample data
+x = np.array([[10], [20], [30], [40], [50]])
+y = np.array([15, 25, 35, 45, 55])
+
+# Model training
+model = LinearRegression()
+model.fit(x, y)
+
+# Prediction
+sample = [[35]]
+prediction = model.predict(sample)
+print(f"Predicted value for {sample[0][0]}: {prediction[0]}")
 
